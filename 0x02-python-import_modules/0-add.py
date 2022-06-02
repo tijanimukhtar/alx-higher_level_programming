8ef8a78fe09a1c598741f06458d23e5206229981
Tijani Mukhtar Akande <tijanimukhtarr@gmail.com>
2022-06-02 16:26:07 +0100

    More Files

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
-import add_0
-
-a = 1
-b = 2
-
-print(a + b)
+#!/usr/bin/python3
+if __name__ == __main__:
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
